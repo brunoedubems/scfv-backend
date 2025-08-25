9bbb02f83a8812f856a348eec7ceee72929bb232
brunoedubems <bemds1@aluno.ifnmg.edu.br>
2025-08-25 19:10:26 -0300

    feat: cria tratamento de erro global

---
+++ src/main/java/br/com/brunoedubems/scfv_backend/exception/EntityNotFoundException.java
@@ -0,0 +1,7 @@
+package br.com.brunoedubems.scfv_backend.exception;
+
+public class EntityNotFoundException extends RuntimeException {
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+}
